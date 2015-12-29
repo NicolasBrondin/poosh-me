16453226aa0684aeaa8d11c588021e4fcc8fa1ec
TheCreatorFr <MrBrondinNicolas@gmail.com>
2015-12-29 02:58:06 +0100

    [FATIGUE] many things

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -88,7 +88,7 @@ angular.module('secureme.controllers', [])
 })
 
 .controller('SettingsCtrl', function($scope,$cordovaSms,$ionicPlatform,storage,$timeout) {
-     $scope.newContact = {key:'call'};
+     $scope.newContact = {key:'sms'};
      $scope.contacts=storage.getContacts();
     console.log($scope.contacts);
          
@@ -122,10 +122,43 @@ angular.module('secureme.controllers', [])
     
     $scope.add = function()
     {
-        $scope.contacts.push($scope.newContact);
-        $scope.newContact = {key:'call'};
-        console.log($scope.contacts);
-        storage.setContacts($scope.contacts);
+        if($scope.contacts.find(function(item,index,array){return item.key=="call";})&&$scope.newContact.key=="call")
+        {
+            
+            $scope.error = "Vous ne pouvez appeler qu'un seul numéro à la fois";
+            $timeout(function(){$scope.error=undefined;},3000);
+        }
+        else if($scope.contacts.find(function(item,index,array){return item.key==$scope.newContact.key&&item.value==$scope.newContact.value;}))
+        {
+            $scope.error = "Vous ne pouvez pas ajouter deux fois le même contact";
+            $timeout(function(){$scope.error=undefined;},3000);
+        }
+        else if(!$scope.newContact.value)
+        {
+            $scope.error = "Ce contact est vide";
+            $timeout(function(){$scope.error=undefined;},3000);
+        }
+        else
+        {
+            if($scope.newContact.key!="mail")
+            {
+                $scope.newContact.value = $scope.newContact.value.replace(/\s+/g, '').replace(/\.+/g, '').replace(/-+/g, '');
+                
+            }
+            if($scope.newContact.key!="mail" && $scope.newContact.value.match(/^[+]?[0-9]+$/))
+            {
+                $scope.contacts.push($scope.newContact);
+                $scope.newContact = {key:'sms'};
+                console.log($scope.contacts);
+                storage.setContacts($scope.contacts);
+            }
+            else
+            {
+                $scope.error = "Un numéro de téléphone ne doit comporter que des chiffres";
+                $timeout(function(){$scope.error=undefined;},3000);
+            }
+        }
+        
     };
 })
 
